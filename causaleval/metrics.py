e17e25adff7dd0dda32885183d3f81d9b49a5c8c
Max <franzmaximilian1996@gmail.com>
2019-07-30 11:07:52 +0200

    consolidate metrics

--- causaleval/metrics.py
+++ causaleval/metrics.py
@@ -15,21 +15,50 @@ class EvaluationMetric():
         """
         self.ex = experiment
 
-    def evaluate(self, data_provider: type(DataProvider), method: type(CausalMethod)):
+    def evaluate(self, data_provider, method):
         """
 
         :param data_provider:
+        :type data_provider: DataProvider
         :param method:
+        :type method: CausalMethod
         :return:
         """
         pass
 
+    def prep_ite(self, data_provider, method):
+        """
 
-class EPEHE(EvaluationMetric):
+        :param data_provider:
+        :type data_provider: DataProvider
+        :param method:
+        :type method: CausalMethod
+        :return:
+        """
+        x, t, y = data_provider.get_training_data()
+        method.fit(x, t, y)
+        return method.predict_ite(x)
+
+
+class StandardErrors(EvaluationMetric):
+    """
+    All the scores that work with full prediction result of the ITE on test data
+    """
 
     @staticmethod
     def pehe_score(true, predicted):
         return np.sqrt(np.mean(np.square(predicted - true)))
+
+    @staticmethod
+    def ate_error(true, predicted):
+        return np.abs(np.mean(true) - np.mean(predicted))
+
+    @staticmethod
+    def enormse(true, predicted):
+        return np.sqrt(np.sum(np.power((1 - predicted/true), 2))/true.shape[0])
+
+    def bias(true, predicted):
+        pass
 
     def evaluate(self, data_provider, method):
         """
@@ -41,21 +70,13 @@ class EPEHE(EvaluationMetric):
         :return:
         """
 
-        x, t, y = data_provider.get_training_data()
-        method.fit(*data_provider.get_training_data())
-        pred_ite = method.predict_ite(x)
-        score = self.pehe_score(data_provider.get_true_ite(), pred_ite)
-        self.ex.log_scalar('pehe score', score)
-        return score
+        pred_ite = self.prep_ite(data_provider, method)
+        true_ite = data_provider.get_true_ite()
+        self.ex.log_scalar('PEHE, {method}, {dataset}'.format(method=str(method), dataset=str(data_provider)), self.pehe_score(true_ite, pred_ite))
+        print('PEHE, {method}, {dataset}'.format(method=str(method), dataset=str(data_provider)), self.pehe_score(true_ite, pred_ite))
+        self.ex.log_scalar('ATE, {method}, {dataset}'.format(method=str(method), dataset=str(data_provider)), self.ate_error(true_ite, pred_ite))
+        print('ATE, {method}, {dataset}'.format(method=str(method), dataset=str(data_provider)), self.ate_error(true_ite, pred_ite))
+        self.ex.log_scalar('ATE, {method}, {dataset}'.format(method=str(method), dataset=data_provider), self.enormse(true_ite, pred_ite))
+        print('enormse score', self.enormse(true_ite, pred_ite))
 
 
-
-    def evaluate_insample(self, data_provider, method):
-        """
-
-        :param data_provider:
-        :type data_provider: DataProvider
-        :param method:
-        :type method: CausalMethod
-        :return:
-        """
