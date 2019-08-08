2df22a3a87ef8f24a3e767a83e7ac2e8e9b59ef9
Max <franzmaximilian1996@gmail.com>
2019-08-08 09:34:54 +0200

    implement propensity score weighting and double robust

---
+++ causaleval/methods/basics/propensity_weighting.py
@@ -0,0 +1,45 @@
+import copy
+import numpy as np
+
+from causaleval.methods.causal_method import CausalMethod
+from causaleval.utils import get_regressor_name
+
+class PropensityScoreWeighting(CausalMethod):
+
+    def __init__(self, propensity_regressor):
+        super().__init__()
+        self.propensity_regressor = propensity_regressor
+
+    def requires_provider(self):
+        return False
+
+    def predict_ate(self, x, t=None, y=None):
+        # Predict ATE always for training set, thus test set evaluation is pretty bad
+        if t is not None and y is not None:
+            # Fit for requested set
+            self.fit(x, t, y)
+            self.x = x
+            self.t = t
+            self.y = y
+
+        prop = self.propensity_regressor.predict(self.x)
+        m1 = np.sum((self.t*self.y/prop) / self.x.shape[0])
+        m0 = np.sum(((1-self.t)*self.y/(1-prop)) / self.x.shape[0])
+        return m1 - m0
+
+    def fit(self, x, t, y, refit=False) -> None:
+        # Fit propensity score model
+        self.x = x
+        self.t = t
+        self.y = y
+        self.propensity_regressor.fit(x, t)
+
+
+    def __str__(self):
+        return "PropensityScoreWeighting"
+
+    def predict_ite(self, x, t=None, y=None):
+        # Broadcast ATE to all instances
+        return np.full(x.shape[0], self.predict_ate(x, t, y))
+
+
