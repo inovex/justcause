eecdc40206604a50b516bd4817e646dfa430dae9
Max <franzmaximilian1996@gmail.com>
2019-08-20 11:09:25 +0200

    Implement methods from causalLearning and R-Learner packages
     - CausalBoosting
     - CausalMARS
     - PTOforest
     - X-leraner (lasso, boost)
     - T-learner (lasso, boost)

---
+++ causaleval/methods/causal_learning.py
@@ -0,0 +1,107 @@
+"""
+All methods provided in https://github.com/saberpowers/causalLearning
+introduced in the paper: ﻿https://arxiv.org/pdf/1707.00102.pdf
+"""
+import os
+os.environ['L_ALL'] = 'en_US.UTF-8'
+os.environ['R_HOME'] = '/usr/local/Cellar/r/3.6.1/lib/R'
+
+
+from rpy2.robjects.packages import importr
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector, FloatVector, IntVector
+import rpy2.robjects as robjects
+from rpy2.robjects import numpy2ri
+
+import numpy as np
+
+from methods.causal_method import CausalMethod
+
+def install_cl():
+    """Load the `causalLearning` R package and activate necessary conversion
+
+    :return: The robject for `causalLearning`
+    """
+
+    # robjects.r is a singleton
+    robjects.r.options(download_file_method='curl')
+    numpy2ri.activate()
+    package_names = ["devtools"]
+    utils = rpackages.importr('utils')
+    utils.chooseCRANmirror(ind=0)
+
+    names_to_install = [x for x in package_names if not rpackages.isinstalled(x)]
+    if len(names_to_install) > 0:
+        utils.install_packages(StrVector(names_to_install))
+
+    return importr('causalLearning')
+
+class CausalLearningMethod(CausalMethod):
+    """ABSTRACT"""
+
+    def __init__(self, seed=0):
+        super().__init__()
+        self.cl = install_cl()
+        self.model = None
+
+    def predict_ite(self, x, t=None, y=None):
+        if self.model is None:
+            raise AssertionError('Must fit the method before prediction')
+
+        return np.array(robjects.r.predict(self.model, x)).reshape(1, -1)[0]
+
+
+class CausalBoosting(CausalLearningMethod):
+
+    def __init__(self, seed=0):
+        super().__init__()
+
+    def __str__(self):
+        return "CausalBoosting"
+
+    def fit(self, x, t, y, refit=False):
+        print('fit causal boost')
+        self.model = self.cl.causalBoosting(x, IntVector(t), FloatVector(y), num_trees=500)
+
+    def predict_ite(self, x, t=None, y=None):
+        if self.model is None:
+            raise AssertionError('Must fit the method before prediction')
+
+        pred = np.array(robjects.r.predict(self.model, x, t, type='treatment.effect'))
+
+        return np.mean(pred, axis=1)
+
+
+class PolinatedTransformedOutcomeForest(CausalLearningMethod):
+    def __init__(self, seed=0):
+        super().__init__()
+
+    def __str__(self):
+        return "PTOforest"
+
+    def fit(self, x, t, y, refit=False):
+        print('fit PTOForest')
+        self.model = self.cl.PTOforest(x, IntVector(t), FloatVector(y))
+
+
+class CausalMars(CausalLearningMethod):
+
+    def __init__(self, seed=0):
+        super().__init__()
+        self.cl = install_cl()
+        self.model = None
+
+    def __str__(self):
+        return "CausalMARS"
+
+    def fit(self, x, t, y, refit=False):
+        print('fit causalMARS')
+        self.model = self.cl.causalMARS(x, IntVector(t), FloatVector(y))
+
+if __name__ == '__main__':
+
+    from data.sets.ihdp import IHDPCfrProvider
+    ihdp = IHDPCfrProvider()
+    cb = CausalBoosting()
+    cb.fit(*ihdp.get_training_data())
+    cb.predict_ite(*ihdp.get_test_data())
---
+++ causaleval/methods/r_learner.py
@@ -0,0 +1,117 @@
+from rpy2.robjects.packages import importr
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector, FloatVector, IntVector
+import rpy2.robjects as robjects
+from rpy2.robjects import numpy2ri
+
+import numpy as np
+
+from methods.causal_method import CausalMethod
+
+class RLearner(CausalMethod):
+    """
+    Uses the R package provided by X.Nie and S. Wager in ﻿https://arxiv.org/pdf/1712.04912.pdf
+    """
+
+    def __init__(self, seed=0, method='lasso'):
+        super().__init__()
+        self.rleaner = self.install_rlearner()
+        self.model = None
+        self.method_name = method
+
+    def __str__(self):
+        return "R-Learner-"+self.method_name.capitalize()
+
+    @staticmethod
+    def install_rlearner():
+        """Load the `rlearner` R package and activate necessary conversion
+
+        :return: The robject for `rlearner`
+        """
+
+        # robjects.r is a singleton
+        robjects.r.options(download_file_method='curl')
+        numpy2ri.activate()
+        package_names = ["devtools"]
+        utils = rpackages.importr('utils')
+        utils.chooseCRANmirror(ind=0)
+
+        names_to_install = [x for x in package_names if not rpackages.isinstalled(x)]
+        if len(names_to_install) > 0:
+            utils.install_packages(StrVector(names_to_install))
+
+        return importr('rlearner')
+
+    def predict_ate(self, x, t=None, y=None):
+        predictions = self.predict_ite(x)
+        return np.mean(predictions)
+
+    def predict_ite(self, x, t=None, y=None):
+        if self.model is None:
+            raise AssertionError('Must fit the forest before prediction')
+
+        return np.array(robjects.r.predict(self.model, x)).reshape(1, -1)[0]
+
+    def fit(self, x, t, y, refit=False):
+        if self.method_name == 'lasso':
+            print('fit lasso')
+            self.model = self.rleaner.rlasso(x, IntVector(t), FloatVector(y))
+        else:
+            # Takes much longer to fit
+            print('fit boost')
+            self.model = self.rleaner.rboost(x, IntVector(t), FloatVector(y))
+
+
+class XLearner(CausalMethod):
+    """
+    Uses the R package provided by X.Nie and S. Wager in ﻿https://arxiv.org/pdf/1712.04912.pdf
+    """
+    def __init__(self, seed=0, method='lasso'):
+        super().__init__()
+        self.rleaner = self.install_rlearner()
+        self.model = None
+        self.method_name = method
+
+    def __str__(self):
+        return "X-Learner-"+self.method_name.capitalize()
+
+    @staticmethod
+    def install_rlearner():
+        """Load the `rlearner` R package and activate necessary conversion
+
+        :return: The robject for `rlearner`
+        """
+
+        # robjects.r is a singleton
+        robjects.r.options(download_file_method='curl')
+        numpy2ri.activate()
+        package_names = ["devtools"]
+        utils = rpackages.importr('utils')
+        utils.chooseCRANmirror(ind=0)
+
+        names_to_install = [x for x in package_names if not rpackages.isinstalled(x)]
+        if len(names_to_install) > 0:
+            utils.install_packages(StrVector(names_to_install))
+
+        return importr('rlearner')
+
+    def predict_ate(self, x, t=None, y=None):
+        predictions = self.predict_ite(x)
+        return np.mean(predictions)
+
+    def predict_ite(self, x, t=None, y=None):
+        if self.model is None:
+            raise AssertionError('Must fit the forest before prediction')
+
+        return np.array(robjects.r.predict(self.model, x)).reshape(1, -1)[0]
+
+    def fit(self, x, t, y, refit=False):
+        if self.method_name == 'lasso':
+            print('fit lasso')
+            self.model = self.rleaner.xlasso(x, IntVector(t), FloatVector(y))
+        else:
+            # Takes much longer to fit
+            print('fit boost')
+            self.model = self.rleaner.xboost(x, IntVector(t), FloatVector(y))
+
+
