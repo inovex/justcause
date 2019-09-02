91c3fc1e522bd92280ff55f4ee665430b4ce7748
Max <franzmaximilian1996@gmail.com>
2019-09-02 08:33:19 +0200

    Add more plots

--- causaleval/utils.py
+++ causaleval/utils.py
@@ -11,7 +11,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
-plt.rcParams['axes.labelsize'] = 4
+plt.rcParams['axes.labelsize'] = 10
 plt.rcParams['figure.facecolor'] = '1'
 plt.rcParams['grid.color'] = 'black'
 plt.rcParams['grid.linestyle'] = ':'
@@ -19,6 +19,8 @@ plt.rcParams['grid.linewidth'] = 0.5
 plt.rcParams["savefig.dpi"] = 300
 
 sns.set_style('whitegrid')
+
+NUM_BINS = 200
 
 def get_regressor_name(representation):
     """
@@ -61,15 +63,26 @@ def surface_plot(y1, y0, y, y_cf, x, name='default'):
     else:
         plt.show()
 
+def ite_plot(y1, y0, write=False, method_name='default'):
+    sns.distplot(y1 - y0, bins=NUM_BINS, color=config.BLUE)
+    plt.tight_layout()
+    if config.PLOT_WRITE:
+        path = config.RESULT_PLOT_PATH
+        plt.savefig(path + '/' + method_name + '-ite')
+        plt.clf()
+    else:
+        plt.show()
 
-def ite_plot(y1, y0):
-    sns.distplot(y1 - y0, bins=100, color=config.BLUE)
-    plt.show()
+def plot_y_dist(y, y_cf, write=False, method_name='default'):
+    sns.distplot(y, bins=NUM_BINS, color=config.BLUE)
+    sns.distplot(y_cf, bins=NUM_BINS, color=config.RED)
+    if config.PLOT_WRITE:
+        path = config.RESULT_PLOT_PATH
+        plt.savefig(path + '/' + method_name + '-ydist')
+        plt.clf()
+    else:
+        plt.show()
 
-def plot_y_dist(y, y_cf):
-    sns.distplot(y, bins=100, color=config.YELLOW)
-    sns.distplot(y_cf, bins=100, color=config.RED)
-    plt.show()
 
 def simple_comparison_mean(y, t):
     treated = y[t==1]
@@ -77,14 +90,54 @@ def simple_comparison_mean(y, t):
     simple_mean = np.mean(treated) - np.mean(control)
     print('simple: ' + str(simple_mean))
 
+def true_ate_plot(true_ates, dataset='default'):
+    sns.set_style('whitegrid')
+    sns.lineplot(x=np.arange(len(true_ates)), y=true_ates, color=config.BLUE, label='True Average Treatment Effect')
+    plt.xlabel('Replications')
+    plt.ylabel('Average Treatment Effect')
+    plt.title(dataset)
+    plt.legend()
+    if config.PLOT_WRITE:
+        path = config.RESULT_PLOT_PATH
+        plt.savefig(path + '/' + dataset + 'true-ate')
+        plt.clf()
+    else:
+        plt.show()
+
+def confounder_outcome_plot(confounder, y_1, dataset='default'):
+    sns.set_style('whitegrid')
+    sns.lineplot(x=confounder, y=y_1, color=config.BLUE, label='Influence of Confounder on Outcome')
+    plt.xlabel('Confounder')
+    plt.ylabel('Treated Outcome')
+    plt.title(dataset)
+    plt.legend()
+    if config.PLOT_WRITE:
+        path = config.RESULT_PLOT_PATH
+        plt.savefig(path + '/' + dataset + 'confounder-outcome')
+        plt.clf()
+    else:
+        plt.show()
+
+def true_ate_dist_plot(true_ates, dataset='default'):
+    sns.set_style('whitegrid')
+    sns.distplot(true_ates, color=config.BLUE, label="Density")
+    plt.title(dataset)
+    plt.legend()
+    if config.PLOT_WRITE:
+        path = config.RESULT_PLOT_PATH
+        plt.savefig(path + '/' + dataset + 'true-ate-dist')
+        plt.clf()
+    else:
+        plt.show()
+
 def robustness_plot(true_ites, predicted_ites, method_name='Method'):
     true_ates = np.mean(true_ites, axis=1)
     predicted_ates = np.mean(predicted_ites, axis=1)
     sns.set_style('whitegrid')
     sns.lineplot(x=np.arange(len(true_ates)), y=true_ates, color=config.BLUE, label='True')
     sns.lineplot(x=np.arange(len(true_ates)), y=predicted_ates, color=config.RED, label='Estimated')
-    plt.xlabel('#runs')
-    plt.ylabel('average treatment effect')
+    plt.xlabel('Replications')
+    plt.ylabel('Average Treatment Effect')
     plt.title(method_name)
     plt.legend()
     if config.PLOT_WRITE:
