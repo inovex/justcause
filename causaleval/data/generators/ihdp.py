c581689c549c83199c26aca6e74957c3bf973709
Max <franzmaximilian1996@gmail.com>
2019-09-09 13:45:00 +0200

    fix the multi-modal setting

--- causaleval/data/generators/ihdp.py
+++ causaleval/data/generators/ihdp.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 from data.data_provider import DataProvider
-from utils import ite_plot, surface_plot, plot_y_dist, simple_comparison_mean, confounder_outcome_plot
+import utils
 
 import config
 
@@ -12,7 +12,7 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def multi_modal_effect(X):
-    prob = np.random.binomial(1, p=sigmoid(X[:, 1]), size=len(X))
+    prob = (sigmoid(X[:, 7]) > 0.5)
     return np.random.normal((3*prob)+1*(1-prob), 0.1, size=len(X)) # Explicitly multimodal
 
 def exponential_effect(X):
@@ -31,6 +31,12 @@ class IHDPGenerator(DataProvider):
     def __str__(self):
         return "IHDP-"+self.setting+"-"+self.base
 
+    def get_training_data(self, size=None):
+        self.load_training_data()
+        self.counter += 1
+        return super(IHDPGenerator, self).get_training_data()
+
+
     def load_training_data(self):
 
         path = config.IHDP_REPLICA_PATH
@@ -38,10 +44,15 @@ class IHDPGenerator(DataProvider):
         filedir = os.path.join(dirname, path)
         all_files = os.listdir(filedir)
 
-        fname = os.path.join(filedir, all_files[0])
+        if self.counter > 110: # IHDP has 1000 replications at max
+            self.counter = 0 # reset counter
+
+
+        fname = os.path.join(filedir, all_files[self.counter])
         data = pd.read_csv(fname)
         covariates_df = data.drop(columns=['mu.0', 'mu.1', 'y', 'z.r'])
         X = covariates_df.values
+
 
         if self.base == 'gamma':
             Y_0 = np.random.gamma(0.2, 1, size=len(X))
@@ -50,7 +61,7 @@ class IHDPGenerator(DataProvider):
 
         if self.setting == 'multi-modal':
             Y_1 = Y_0 + multi_modal_effect(X)
-            val = X[:, 1]
+            val = X[:, 7]
         elif self.setting == 'small-exponential':
             Y_1 = Y_0 + small_exponential_effect(X)
             val = X[:, 7]
@@ -58,7 +69,7 @@ class IHDPGenerator(DataProvider):
             Y_1 = Y_0 + exponential_effect(X)
             val = X[:, 7]
 
-        self.x = X
+        self.x = X[:, 0:8]
         self.y_1 = Y_1
         self.y_0 = Y_0
 
@@ -71,4 +82,5 @@ class IHDPGenerator(DataProvider):
 if __name__ == '__main__':
     ihdp = IHDPGenerator(setting='multi-modal', base='non-gamma')
     ihdp.load_training_data()
-    confounder_outcome_plot(ihdp.x[:, 1], ihdp.y_1 - ihdp.y_0, dataset='IDHP-Multi-Modal')
+    utils.confounder_outcome_plot(ihdp.x[:, 7], ihdp.y_1 - ihdp.y_0, dataset='IDHP-Multi-Modal')
+    utils.dist_plot(ihdp.x[:, 7], method_name='ihdp-confounder')
