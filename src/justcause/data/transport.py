81f711975f0565d8ef15af13706e262b2354044d
Max <franzmaximilian1996@gmail.com>
2019-10-15 11:21:43 +0200

    rename for clarity; add comments

--- src/justcause/data/transport.py
+++ src/justcause/data/transport.py
@@ -42,7 +42,22 @@ def download(url, dest_path):
             fd.write(chunk)
 
 
-def get_data(url, dest_subdir, dest_filename, download_if_missing=True):
+def get_local_data_path(url, dest_subdir, dest_filename, download_if_missing=True):
+    """ Downloads the file from url if necessary; returns local file path
+
+    If the requested local file does not exist, it is downloaded form the url and the
+    local path is returned
+
+    Args:
+        url: url from where to download
+        dest_subdir: name of the subdirectory
+        dest_filename: name of the file
+        download_if_missing: download the
+
+    Returns: path to the file
+    Raises: IOError if file does not exist and download is set to False
+
+    """
 
     data_dir = os.path.join(os.path.abspath(DATA_DIR), dest_subdir)
 
@@ -62,7 +77,7 @@ def get_data(url, dest_subdir, dest_filename, download_if_missing=True):
 def load_parquet_dataset(base_url, dest_subdir):
     """ Load dataset stored in parquet at the specified directory
 
-    Takes advantage of the efficient storage format
+    Takes advantage of the efficient storage format parquet
 
     Args:
         base_url: base url where the files can be found
@@ -74,8 +89,12 @@ def load_parquet_dataset(base_url, dest_subdir):
     COV_URL = os.path.join(base_url, COV_FILE)
     REP_URL = os.path.join(base_url, REP_FILE)
 
-    cov_path = get_data(COV_URL, dest_subdir, COV_FILE, download_if_missing=True)
+    cov_path = get_local_data_path(
+        COV_URL, dest_subdir, COV_FILE, download_if_missing=True
+    )
     cov = pd.read_parquet(cov_path)
-    rep_path = get_data(REP_URL, dest_subdir, REP_FILE, download_if_missing=True)
+    rep_path = get_local_data_path(
+        REP_URL, dest_subdir, REP_FILE, download_if_missing=True
+    )
     rep = pd.read_parquet(rep_path)
     return cov, rep
