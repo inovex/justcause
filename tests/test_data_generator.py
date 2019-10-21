259e267c3986b0a1bddb46631b7fa96d44117809
Max <franzmaximilian1996@gmail.com>
2019-10-21 10:40:29 +0200

    minimal test for toy-dgp

--- tests/test_data_generator.py
+++ tests/test_data_generator.py
@@ -1,4 +1,5 @@
 from justcause.data.generators.ihdp import multi_expo_on_ihdp
+from justcause.data.generators.toy import toy_example_dgp, toy_example_emcs
 
 
 def test_ihdp_generator():
@@ -11,3 +12,26 @@ def test_ihdp_generator():
 
     ihdp = multi_expo_on_ihdp(setting="exponential", size=200, replications=100)
     assert len(ihdp.data) == 200 * 100
+
+
+def test_toy_generator():
+    size = 10000
+    replications = 5
+
+    toy = toy_example_dgp(setting="simple", size=size, replications=replications)
+    assert len(toy.data) == size * replications
+    assert len(toy.data.groupby("rep")) == replications
+
+    size = 100
+    replications = 50
+
+    toy = toy_example_dgp(setting="hard", size=size, replications=replications)
+    assert len(toy.data) == size * replications
+    assert len(toy.data.groupby("rep")) == replications
+
+    size = 10000
+    replications = 10
+
+    toy = toy_example_emcs(setting="simple", size=size, replications=replications)
+    assert len(toy.data) == size * replications
+    assert len(toy.data.groupby("rep")) == replications
