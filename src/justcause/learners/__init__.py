c93f7d47da2ef9c352a08bdb25aeb625e9b8f84f
Max <franzmaximilian1996@gmail.com>
2019-11-05 12:17:55 +0100

    ignore unused imports in module __init__.py

--- src/justcause/learners/__init__.py
+++ src/justcause/learners/__init__.py
@@ -1,6 +1,6 @@
-from .meta.slearner import SLearner, WeightedSLearner
-from .meta.tlearner import TLearner, WeightedTLearner
-from .meta.rlearner import RLearner
-from .meta.xlearner import XLearner
+from .meta.slearner import SLearner, WeightedSLearner  # noqa: F401
+from .meta.tlearner import TLearner, WeightedTLearner  # noqa: F401
+from .meta.rlearner import RLearner  # noqa: F401
+from .meta.xlearner import XLearner  # noqa: F401
 
-from .tree.causal_forest import CausalForest
+from .tree.causal_forest import CausalForest  # noqa: F401
