c3c629d8e2d71d5094f49500e8aee81229223090
Florian Wilhelm <Florian.Wilhelm@inovex.de>
2019-11-07 16:35:12 +0100

    Set __all__ in __init__.py

--- src/justcause/learners/__init__.py
+++ src/justcause/learners/__init__.py
@@ -2,5 +2,14 @@ from .meta.slearner import SLearner, WeightedSLearner  # noqa: F401
 from .meta.tlearner import TLearner, WeightedTLearner  # noqa: F401
 from .meta.rlearner import RLearner  # noqa: F401
 from .meta.xlearner import XLearner  # noqa: F401
-
 from .tree.causal_forest import CausalForest  # noqa: F401
+
+___all__ = [
+    "SLearner",
+    "WeightedSLearner",
+    "TLearner",
+    "WeightedTLearner",
+    "RLearner",
+    "XLearner",
+    "CausalForest",
+]
