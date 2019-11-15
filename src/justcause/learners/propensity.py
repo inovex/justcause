33365cafa9e779837c117018735a1af4e5012b0d
Max <franzmaximilian1996@gmail.com>
2019-11-15 12:03:59 +0100

    implement basic propensity defaults and helpers

--- src/justcause/learners/propensity.py
+++ src/justcause/learners/propensity.py
@@ -1,4 +1,5 @@
 from causalml.propensity import ElasticNetPropensityModel
+from pygam import LogisticGAM, s
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.linear_model import LogisticRegression
 
@@ -8,11 +9,11 @@ def get_default_estimator():
     return LogisticRegression()
 
 
-def get_default_propensities(x, t):
-    """ Estimates propensity score using the default estimator"""
+def estimate_propensities(x, t):
+    """ Estimates and calibrates propensity score using the default estimator and GAM"""
     learner = get_default_estimator()
     learner.fit(x, t)
-    return learner.predict_proba(x)[:, 1]
+    return calibrate(learner.predict_proba(x)[:, 1], t)
 
 
 # TODO: Consider https://scikit-learn.org/stable/modules/calibration.html
@@ -20,13 +21,27 @@ def get_default_propensities(x, t):
 def set_propensity_learner(p_learner=None):
     """ Returns default propensity learner or checks the given p_learner"""
     if p_learner is None:
-        return ElasticNetPropensityModel()
+        return get_default_estimator()
     else:
         assert hasattr(
             p_learner, "predict_proba"
         ), "propensity learner must have predict_proba method"
 
-        return CalibratedClassifierCV(p_learner)
+        return CalibratedClassifierCV(p_learner, method="isotonic")
+
+
+def calibrate(propensities, treatment):
+    """
+    Post-hoc calibration of propensity scores given the true treatments
+
+    Args:
+        propensities: propensity scores
+        treatment: treatment indicator
+
+    Returns: calibrated probabilites of treatment
+    """
+    gam = LogisticGAM(s(0)).fit(propensities, treatment)
+    return gam.predict_proba(propensities)
 
 
 # TODO: This is really only required when the ElasticNetPropensityModel is used
