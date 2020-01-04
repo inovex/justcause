c0c9d1c5bc576f48a51258bac2438f1eecd6b32e
Max <franzmaximilian1996@gmail.com>
2020-01-04 12:22:13 +0100

    cleanup ate estimators and the use of propensity

--- src/justcause/learners/ate/double_robust.py
+++ src/justcause/learners/ate/double_robust.py
@@ -4,11 +4,11 @@ from typing import Optional
 import numpy as np
 from sklearn.linear_model import LassoLars
 
-from ..propensity import fit_predict_propensity, set_propensity_learner
+from ..propensity import estimate_propensities
 
 
-class DoubleRobustEstimator:
-    """ Implements Double Robust Estmation with generic learners based on the
+class DoubleRobustEstimator(object):
+    """Implements Double Robust Estmation with generic learners based on the
         equations of M. Davidian
 
 
@@ -27,16 +27,16 @@ class DoubleRobustEstimator:
         learner_t=None,
         delta=0.001,
     ):
-        """
+        """Inits the DoubleRobustEstimator
 
         Args:
-            propensity_learner: propensity regression model, default set in
-                the set_propensity_learner method in justcause.learners.utils
-            learner: generic outcome learner for both outcomes
-            learner_c: specific control outcome learner
-            learner_t: specific treatment outcome learner
+            propensity_learner: a classifier model with probability estimation method
+                `predict_proba` like a sklearn LogisticRegression
+            learner: generic outcome regression model for both outcomes
+            learner_c: specific control outcome regression model
+            learner_t: specific treatment outcome regression model
         """
-        self.propensity_learner = set_propensity_learner(propensity_learner)
+        self.propensity_learner = propensity_learner
 
         if learner is None:
             if learner_c is None and learner_t is None:
@@ -53,7 +53,7 @@ class DoubleRobustEstimator:
         self.delta = delta
 
     def __str__(self):
-        """ Simple string representation for logs and outputs"""
+        """Simple string representation for logs and outputs"""
         return "{}(control={}, treated={}, propensity={})".format(
             self.__class__.__name__,
             self.learner_c.__class__.__name__,
@@ -68,25 +68,29 @@ class DoubleRobustEstimator:
         y: np.array,
         propensity: Optional[np.array] = None,
     ) -> float:
-        """ Fits learners and estimates average treatment
-        effect of the given population
+        """Estimates average treatment effect of the given population
 
         Args:
             x: covariates in shape (num_instances, num_features)
-            t: treatment indicator
-            y: factual outcomes
+            t: binary treatment indicator vector of shape (num_instances)
+            y: factual outcomes of shape (num_instances)
             propensity: explicit propensity scores of all instances to be used for
-                weighting in the formula
+                weighting in the double robust estimation formula
 
         Returns:
+            ate: estimate of the average treatment effect for the population
 
         """
 
         self._fit(x, t, y)
 
-        # predict propensity
         if propensity is None:
-            propensity = fit_predict_propensity(self.propensity_learner, x, t)
+            # estimate propensity if not given
+            if self.propensity_learner is None:
+                propensity = estimate_propensities(x, t)
+            else:
+                self.propensity_learner.fit(x, t)
+                propensity = self.propensity_learner.predict_proba(x)[:, 1]
 
         dr1 = (
             np.sum(
@@ -107,6 +111,6 @@ class DoubleRobustEstimator:
         return float(dr1 - dr0)
 
     def _fit(self, x: np.array, t: np.array, y: np.array) -> None:
-        """ Fits the outcome learners on treated and control separately"""
+        """Helper to fit the outcome learners on treated and control separately"""
         self.learner_c.fit(x[t == 0], y[t == 0])
         self.learner_t.fit(x[t == 1], y[t == 1])
--- src/justcause/learners/ate/propensity_weighting.py
+++ src/justcause/learners/ate/propensity_weighting.py
@@ -2,15 +2,22 @@ from typing import Optional
 
 import numpy as np
 
-from ..propensity import fit_predict_propensity, set_propensity_learner
+from ..propensity import estimate_propensities
 
 
-class PSWEstimator:
-    """ Implements the simple propensity score weighting estimator """
+class PSWEstimator(object):
+    """Implements the simple propensity score weighting estimator
+
+    To estimate the average treatment effect, the inverse propensity weighted
+    mean outcomes of treated and control are calculated and subtracted.
+
+    References:
+        [1] http://freerangestats.info/blog/2017/04/09/propensity-v-regression
+
+    """
 
     def __init__(self, propensity_learner=None, delta=0.001):
-        self.propensity_learner = set_propensity_learner(propensity_learner)
-
+        self.propensity_learner = propensity_learner
         self.delta = delta
 
     def __str__(self):
@@ -25,12 +32,28 @@ class PSWEstimator:
         y: np.array,
         propensities: Optional[np.array] = None,
     ) -> float:
-        """ Fits the models on given population and calculates the ATE"""
+        """Estimates average treatment effect of the given population
+
+        Args:
+            x: covariate matrix of the population of shape
+                (num_instances, num_covariates)
+            t: binary treatment indicator vector of shape (num_instances)
+            y: factual outcome vector of shape (num_instances)
+            propensities:
+
+        Returns:
+            ate: Estimate of the average treatment effect across the population
+
+        """
 
         num_samples = x.shape[0]
 
         if propensities is None:
-            propensities = fit_predict_propensity(self.propensity_learner, x, t)
+            if self.propensity_learner is None:
+                propensities = estimate_propensities(x, t)
+            else:
+                self.propensity_learner.fit(x, t)
+                propensities = self.propensity_learner.predict_proba(x)[:, 1]
 
         m1 = np.sum(((t * y + self.delta) / (propensities + self.delta)))
         m0 = np.sum((((1 - t) * y + self.delta) / (1 - propensities + self.delta)))
